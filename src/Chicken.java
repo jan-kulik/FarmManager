9251877077530d5faae984e217d396e750ba3c1f
Jan <217564589+jan-kulik@users.noreply.github.com>
2026-03-06 01:08:14 +0100

    add Chicken class to represent chicken animals with specific properties and behaviors

---
+++ src/Chicken.java
@@ -0,0 +1,45 @@
+public class Chicken extends Animal {
+
+    public Chicken(int id, String name) {
+        super(id, name, 0, 100, 0);
+    }
+
+    public Chicken(int id, String name, int ageDays, int hunger, int productionCounterDays) {
+        super(id, name, ageDays, hunger, productionCounterDays);
+    }
+
+    @Override
+    public AnimalType getType() {
+        return AnimalType.CHICKEN;
+    }
+
+    @Override
+    public double getDailyFeedCost() {
+        return 1.5;
+    }
+
+    @Override
+    public int getDailyHungerLoss() {
+        return 10;
+    }
+
+    @Override
+    public int getMinHungerToProduce() {
+        return 40;
+    }
+
+    @Override
+    public String getProductItemId() {
+        return "eier";
+    }
+
+    @Override
+    public int getProductAmount() {
+        return 1;
+    }
+
+    @Override
+    public int getProductionIntervalDays() {
+        return 1;
+    }
+}
