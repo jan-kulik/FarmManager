03b0933f485eca4c011845401c98d98a30557520
Jan <217564589+jan-kulik@users.noreply.github.com>
2026-03-20 16:04:30 +0100

    update item display format to include unit of measurement in inventory listing

--- src/App.java
+++ src/App.java
@@ -64,7 +64,7 @@ public class App {
                 System.out.println("Freie Kapazität: " + inventory.getFreeCapacity());
             }
             for (String id : inventory.getItemIdsSorted()) {
-                System.out.println("- " + inventory.getDisplayName(id) + ": " + inventory.getAmount(id) + " / " + inventory.getMaxItemCapacity(id) + inventory.getAmount(id));
+                System.out.println("- " + inventory.getDisplayName(id) + ": " + inventory.getAmount(id) + " / " + inventory.getMaxItemCapacity(id) + inventory.getUnit(id));
             }
             System.out.print("Enter drücken, um zum Menü zurückzukehren.");
             sc.nextLine();
